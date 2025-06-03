3bc5edb9e7b3ba9e1a53b18af4b385973978d789
ahankish <alexash.college@gmail.com.>
2025-06-03 11:55:17 -0700

    using v3 of test_random for test_wire.sh

--- test_wire.sh
+++ test_wire.sh
@@ -8,7 +8,7 @@ echo $? # if the exit status is 1, then either the directory exists, or there's 
 # changing the wire parameters - hard-coded (only 2 values per iteration):
 
 # compiling the file ???
-g++ -o test_random_v2 test_random_v2.C $( root-config --libs --cflags )
+g++ -o test_random_v3 test_random_v3.C $( root-config --libs --cflags )
 
 # running for inital parameter values 
 for (( n=10 ; n<21 ; n=n+10 ));
@@ -19,7 +19,7 @@ do
     do 
       for (( k=1 ; k<3 ; k++)); 
       do 
-        ./test_random_v2 $n $i $j $k 
+        ./test_random_v3 $n $i $j $k 
       done
     done
   done
