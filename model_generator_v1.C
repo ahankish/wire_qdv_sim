f48a5da8e6be7f1c348c8c52864aac58f37819a9
ahankish <alexash.college@gmail.com.>
2026-01-07 11:45:02 -0700

    updates

--- model_generator_v1.C
+++ model_generator_v1.C
@@ -57,13 +57,17 @@ void make_histo_pretty_qdvpos(TH1D* h){
 void model_generator_v1(double termination_resN, double termination_resS, double north_gain, double south_gain,
                         std::string filename, int wirepos) {
 
+
   TDirectory::AddDirectory(kFALSE); // setting directory to 0 for all histograms 
 
-  if ((north_gain == 0) || (south_gain == 0)) {
+  if ((north_gain <= 0) || (south_gain <= 0)) {
     //std::cout << "Gain cannot be negative or equal to 0. " << std::endl;
     //std::cout << "Gain automatically set to 1. " << std::endl;
     north_gain = 1;
+    south_gain = 1;
   }
+
+  /*
 
   // strings for making filenames
   std::ostringstream termresNstr, termresSstr, gain_str, wire_pos;
@@ -85,6 +89,32 @@ void model_generator_v1(double termination_resN, double termination_resS, double
                             + underscore + gainstr;
 
   std::string path = folder + prefix + file_params + root_str;
+  //std::string path = prefix + file_params + root_str;
+
+  */
+  auto term_ResN = make_unique<std::string>(std::to_string(termination_resN));
+  auto term_ResS = make_unique<std::string>(std::to_string(termination_resS));
+  auto gain_Ratio = make_unique<std::string>(std::to_string(north_gain/south_gain));
+  auto wirelo = make_unique<std::string>(std::to_string(wirepos));
+
+  std::string file_params;
+  file_params.append(*term_ResN);
+  file_params += "_";
+  file_params.append(*term_ResS);
+  file_params += "_";
+  file_params.append(*gain_Ratio);
+
+
+  std::string path("wiresim_files");
+  path.append(*wirelo);
+  path.append("/");
+  path += "wire_";
+  path.append(*term_ResN);
+  path += "_";
+  path.append(*term_ResS);
+  path += "_";
+  path.append(*gain_Ratio);
+  path += ".root";
 
 
 //======================================================================================================================
@@ -96,6 +126,11 @@ void model_generator_v1(double termination_resN, double termination_resS, double
    std::cerr << "Error opening file " << filename << std::endl;
    exit(-1);
   }
+  /*
+  else {
+    std::cout << "Successfully opened file: " << filename << std::endl;
+  }
+  */
 
   //TApplication *app = new TApplication("app", 0, 0);
   gROOT->SetBatch(kTRUE);
@@ -106,6 +141,7 @@ void model_generator_v1(double termination_resN, double termination_resS, double
   TH1D* proj1 = proj0->ProjectionX("test1", wirepos, wirepos); // 1D histogram of hit pos. likelihood based on z pos.
   double min_val = proj1->GetBinCenter(proj1->FindFirstBinAbove(0));
   
+
   std::string proj_name = "cDCTproj" + file_params;
   TCanvas* cDCTproj = new TCanvas(proj_name.c_str(), "X Projection - 1D Histogram");
   cDCTproj->SetLeftMargin(0.15);
@@ -120,7 +156,8 @@ void model_generator_v1(double termination_resN, double termination_resS, double
 
 
 // graphing the histogram(s)
-  int num_entries=1000000;
+  int num_entries=10000000; // 8/18: Increased from 1_000_000 to 10_000_000 entries
+  //int num_entries=100000000; // 8/18: Increased from 10_000_000 to 100_000_000 entries
   gROOT->Reset();
   TStyle * plain = new TStyle("plain","plain");
   plain->SetCanvasBorderMode(0);
@@ -134,13 +171,17 @@ void model_generator_v1(double termination_resN, double termination_resS, double
   gStyle->SetPalette(1);
 
   // min and max chargediv can be used to first sample for fraction
-  std::string h_div_name = "Charge Division " + file_params;
-  std::string h_true_pos_name = "True Position " + file_params; 
-  std::string h_calcqdv_name = "Charge Division calculated " + file_params;
-  
+  //std::string h_div_name = "Charge Division " + file_params;
+  //std::string h_true_pos_name = "True Position " + file_params; 
+  //std::string h_calcqdv_name = "Charge Division calculated " + file_params;
+
   TH1D * h_div = new TH1D("Charge Division", "", 200,-0.05,1.05);  
   TH1D * h_true_pos = new TH1D("True Position", "", 200,-1.1*DCT_wire_length/2.0,1.1*DCT_wire_length/2.0); //*********** */
   TH1D * h_calcqdv = new TH1D("Charge Division calculated", "", 200,-0.05,1.05);  
+
+  //TH1D * h_div = new TH1D(h_div_name, "", 200,-0.05,1.05);  
+  //TH1D * h_true_pos = new TH1D(h_true_pos_name, "", 200,-1.1*DCT_wire_length/2.0,1.1*DCT_wire_length/2.0); //*********** */
+  //TH1D * h_calcqdv = new TH1D(h_calcqdv_name, "", 200,-0.05,1.05);  
 
   //disable display of histogram statistics
   h_div->SetStats(false);
@@ -166,12 +207,11 @@ void model_generator_v1(double termination_resN, double termination_resS, double
     double total_resS=res_south+termination_resS;
         
     double qdv_new=(north_gain/total_resN)/((north_gain/total_resN)+(south_gain/total_resS));
-
     h_calcqdv->Fill(qdv_new);
   }
 
   // now apply a voltage to this
-  
+  /*
   TCanvas * c = new TCanvas(h_div_name.c_str(),"Calculated Wire Charge Distribution", 200,10,600,600);
   c->SetLeftMargin(0.15);
   c->SetRightMargin(0.04);
@@ -185,7 +225,7 @@ void model_generator_v1(double termination_resN, double termination_resS, double
 
   //h_calcqdv->Draw();
   //h_div->Draw("SAME");
-  
+
   // c->SaveAs("GainRatio_resistance_model.png"); // SAVING THE HISTOGRAM AS A PNG 
 
   TCanvas * c2 = new TCanvas(h_calcqdv_name.c_str(),"Original Wire Charge Distribution", 200,50,600,600);
@@ -193,27 +233,18 @@ void model_generator_v1(double termination_resN, double termination_resS, double
   c2->SetRightMargin(0.04);
   c2->SetTopMargin(0.04);
   make_histo_pretty_qdvpos(h_true_pos);
-
-  /*
-  TCanvas * cTruePos = new TCanvas("c_truepos_ref","True Position", 200,10,600,600);
-  cTruePos->SetLeftMargin(0.15);
-  cTruePos->SetRightMargin(0.04);
-  cTruePos->SetTopMargin(0.04);
-  cTruePos->cd();
-  h_true_pos->Draw();
   */
+
 
   //app->Run(); // running all graphics using TApplication
 
   // saving the histogram: 
-  //std::string term_resN_str = std::to_string(termination_resN);
-  //std::string term_resS_str = std::to_string(termination_resS);
   float gain_ratio = north_gain / south_gain;
   if (std::isinf( gain_ratio )) {
     std::cout << "Gain Ratio cannot be infinite. Aborting model creation." << std::endl;
   }
 
-  
+
   else {
     const char *path_c = path.c_str();
     std::unique_ptr<TFile> myFile( TFile::Open(path_c, "RECREATE") );
