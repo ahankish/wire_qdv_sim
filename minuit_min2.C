5409b88888a049292c6d155dfdaf3f9d8608ae3f
ahankish <alexash.college@gmail.com.>
2026-01-14 12:43:10 -0700

    Fixed random number generation

--- minuit_min2.C
+++ minuit_min2.C
@@ -11,7 +11,7 @@
 #include "Math/IFunctionfwd.h"
 #include "model_generator_v1.C"
 #include <vector>
-#include "TRandom.h"
+#include "TRandom3.h"
 #include "TCanvas.h"
 #include "TH1D.h"
 #include "TApplication.h"
@@ -25,9 +25,10 @@ int EntryNumber = 0; // To keep track of Minuit attempt - for plotting efficacy
 
 // Plotting Residuals: 
 std::unique_ptr<TCanvas> newCanvas(new TCanvas("randComp","Residuals of Minimization Parameter", 200,50,600,600));
-
-// Storing the residual values 
-std::unique_ptr<TH1D> resPlt(new TH1D("residuals","Residuals of Minimization Parameter", 100, -50, 50));
+std::unique_ptr<TH1D> resPlt(new TH1D("residuals","Residuals of Minimization Parameter", 100, 0, 50000));
+//std::unique_ptr<TFile> residualFile( TFile::Open("residuals_plot.root", "RECREATE") );
+TRandom3* rNum = new TRandom3(0);
+//double randInputs[3] = {0.1 + (rNum->Rndm() * 299.9), 0.1 + (rNum->Rndm() * 299.9), 0.1 + (rNum->Rndm() * 9.9)};
 
 
 // Derived class from IBaseFunctionMultiDim
@@ -99,7 +100,7 @@ class FCNClass : public ROOT::Math::IMultiGenFunction {
     std::cout << "Chi^2 Result = " << chi2/num_bins << "\n"<< std::endl;
 
     // Adding to residuals plot: 
-    resPlt->Fill(chi2/num_bins); // predicted value - observed value
+    resPlt->Fill(chi2/num_bins); 
 
     // Marking Entry Number and adding to efficacy plot 
     EntryNumber++;
@@ -117,6 +118,13 @@ void minuit_min2(int wirepos) {
   //Minuit2Minimizer* min = new Minuit2Minimizer();
   ROOT::Minuit2::Minuit2Minimizer* min = new ROOT::Minuit2::Minuit2Minimizer();
 
+  double *const randInputs = new double[3];
+  randInputs[0] = 0.1 + (rNum->Rndm() * 299.9);
+  randInputs[1] = 0.1 + (rNum->Rndm() * 299.9);
+  randInputs[2] = 0.1 + (rNum->Rndm() * 9.9);
+  std::cout << randInputs[0] << ", " << randInputs[1] << ", " << randInputs[2] << std::endl;
+
+
   // Instance of the Strategy class MnStrategy
   ROOT::Minuit2::MnStrategy* strat = new ROOT::Minuit2::MnStrategy();
   strat->SetHighStrategy(); // the minimizer will waste no resources looking for the minimum
@@ -131,9 +139,21 @@ void minuit_min2(int wirepos) {
   double variable[3] = {5,20,0.9};
   double lower[3] = {0.0000001, 0.0000001, 0.0000001};
   double upper[3] = {300, 300, 10};
+
   
   // Generate random values for the fixed variables
-  double rand[3] = {gRandom->Uniform(0.0000001,300), gRandom->Uniform(0.0000001,300), gRandom->Uniform(0.0000001,10)};
+  //TRandom3* rNum = new TRandom3();
+  //auto northRand = rNum->Rndm();
+  //auto southRand = rNum->Rndm();
+  //auto gainRand = rNum->Rndm();
+
+  //double rand[3] = {gRandom->Uniform(0.0000001,300), gRandom->Uniform(0.0000001,300), gRandom->Uniform(0.0000001,10)};
+  //double rand[3] = {299.999999*northRand + 0.0000001, 299.999999*southRand + 0.0000001, 9.999999*gainRand + 0.0000001};
+  //double randInputs[3] = {0.1 + (rNum->Rndm() * 299.9), 0.1 + (rNum->Rndm() * 299.9), 0.1 + (rNum->Rndm() * 9.9)};
+
+  //std::cout << southRand << " is the random south termination resistance" << std::endl;
+  //std::cout << gainRand << " is the random gain ratio" << std::endl;
+
 /*
   min->SetLimitedVariable(0, "north termination resistance", variable[0], step[0], lower[0], upper[0]); 
   min->SetLimitedVariable(1, "south termination resistance",variable[1], step[1], lower[1], upper[1]);
@@ -142,9 +162,13 @@ void minuit_min2(int wirepos) {
 */
 
   min->SetLimitedVariable(0, "north termination resistance", variable[0], step[0], lower[0], upper[0]); 
-  min->SetFixedVariable(1, "south termination resistance", rand[1]);
-  min->SetFixedVariable(2, "gain ratio", rand[2]);
+  min->SetFixedVariable(1, "south termination resistance", randInputs[1]);
+  min->SetFixedVariable(2, "gain ratio", randInputs[2]);
   min->SetFixedVariable(3, "wire location", wirepos); // position of the wire in the dct
+
+
+  //TApplication *app = new TApplication("app", 0, 0);
+  //gSystem->ProcessEvents(); // for getting the graphics to display
 
 
   bool min_status = min->Minimize();
@@ -158,14 +182,23 @@ void minuit_min2(int wirepos) {
   std::cout << "South termination resistance minimum: " << mins[1] << std::endl;
   std::cout << "Gain ratio minimum: " << mins[2] << std::endl;
 
+  
+  // Saving residuals plot to a root file
+  std::unique_ptr<TFile> residualFile( TFile::Open("residuals_plot.root", "RECREATE") );
+  resPlt->Write();
+  //residualFile->WriteObject(&resPlt, "residuals plot");
+
+
+  //resPlt->Draw("randComp"); // drawing the residuals plot on the canvas
+  //newCanvas->SaveAs("residuals_plot.png"); // saving the plot as a png file
+  //app->Run();
+
 
   // Deallocating Memory
   delete min;
   delete strat;
   delete &f;
-
-  resPlt->DrawClone(); // drawing the residuals plot on the canvas4
-  newCanvas->SaveAs("residuals_plot.jpg"); // saving the plot as a jpg file
+  delete [] randInputs;
 
 }
 
